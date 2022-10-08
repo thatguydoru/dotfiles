7036edcd50ea0ac1e3d4708d1f7112e684a97bc8
thatguydoru <jtorres.doru@gmail.com>
2022-10-09 07:18:25 +0800

    explicit theme integration for telescope

--- nvim/lua/doru/theme.lua
+++ nvim/lua/doru/theme.lua
@@ -2,6 +2,7 @@ vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
 require("catppuccin").setup({
     integrations = {
         neotree = true,
+        telescope = true,
     }
 })
 vim.api.nvim_command "colorscheme catppuccin"
