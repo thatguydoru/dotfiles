611e105cb22527595566091d61e7ea183f553597
thatguydoru <jtorres.doru@gmail.com>
2022-10-16 13:47:38 +0800

    lsp icons and added package list backup

--- nvim/lua/doru/lsp/lspconfig.lua
+++ nvim/lua/doru/lsp/lspconfig.lua
@@ -12,3 +12,9 @@ for _, lsp in ipairs(servers) do
         capabilities = capabilities,
     }
 end
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
