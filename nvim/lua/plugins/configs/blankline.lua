63934fc4e518f1c5f433ed501408d56d4ef474bf
thatguydoru <jtorres.doru@gmail.com>
2022-10-31 10:06:52 +0800

    minor changes

--- nvim/lua/plugins/configs/blankline.lua
+++ nvim/lua/plugins/configs/blankline.lua
@@ -1,7 +1,7 @@
 require "indent_blankline".setup {
     show_end_of_line = true,
     space_char_blankline = " ",
-    char_list = {'│', '|', '¦', '┆'},
+    --char_list = {'│', '|', '¦', '┆'},
     --char_highlight_list ={
     --    "IndentBlanklineIndent1",
     --    "IndentBlanklineIndent2",
